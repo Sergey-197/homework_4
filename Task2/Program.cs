f74fa7ffa16b8accd80f2e5ab63c6a6f3e12f3ad
Sergey <spumen35@gmail.com>
2023-12-17 16:50:39 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задайте массив заполненный случайными трёхзначными числами. 
+//Напишите программу, которая покажет 
+//количество чётных чисел в массиве
+
+// пример
+
+// [344 452 341 125] => 2
+
+
+static void Main()
+{
+    Console.WriteLine("Введите размер нашего массива");
+    int numbers = Convert.ToInt32(Console.ReadLine());
+    Random random = new Random();
+    int сount = 0;
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = random.Next(100, 1000);
+        System.Console.Write(numbers[i] + " ");
+    }
+    if (numbers[i] % 2 == 0)
+    {
+        сount++;
+    }
+    Console.WriteLine($"\nКоличество четных чисел в массиве: {сount}");
+    {
+}
+
